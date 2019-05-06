aa22b3f522c3af833224bdb945ebf5b3cd3f0bc0
antaluqi <33272769+antaluqi@users.noreply.github.com>
2019-05-06 15:10:38 +0800

    增加down20的平缓度描述

--- FIND.sql
+++ FIND.sql
@@ -2,8 +2,9 @@
 -- cc:code
 -- rch:rate of channl
 -- rb rate of c_down20 (point of buy)
-CREATE OR REPLACE FUNCTION public.find(cc text, rch integer, rb real)
- RETURNS TABLE(code text, date date, rchannl real, buy real, low real, c_down20 real, rlhh2 real, rlhh5 real, rlhh10 real, rc2 real, rc5 real, rc10 real)
+
+CREATE OR REPLACE FUNCTION public.find(cc text, rch integer, rb real, dnlag integer)
+ RETURNS TABLE(code text, date date, rchannl real, rdown20 real, buy real, low real, c_down20 real, rlhh2 real, rlhh5 real, rlhh10 real, rc2 real, rc5 real, rc10 real)
  LANGUAGE plpgsql
  STRICT
 AS $function$
@@ -11,12 +12,18 @@ declare
 begin
     -- -------------------------------------------------------------------------------------------
        return query 
-		with m as (select lag(maboll.date,-1) over(order by maboll.date) as tdate,maboll.code,(up20-down20)/down20*100 as rchannl from maboll where maboll.code=cc),  
+		with m as (select lag(maboll.date,-1) over(order by maboll.date) as tdate,
+		                  maboll.code,
+		                  maboll.down20,
+		                  (maboll.up20-maboll.down20)/maboll.down20*100 as rchannl,
+	                      lag(maboll.down20,dnlag) over(order by maboll.date) as down20_lag
+	               from maboll where maboll.code=cc),  
 		     g as (select golang.date,golang.code,golang.low from golang where golang.code=cc),
 		     c as (select predict.date,predict.code,predict.c_down20,predict.c_down20*(1-rb/100) as buy from predict where predict.code=cc),
 		     f as (select future.date,future.code,lhh2,lhh5,lhh10,c2,c5,c10 from future where future.code=cc)
 		select g.code,g.date,
 		       m.rchannl:: real,
+		       (m.down20-m.down20_lag)/m.down20_lag*100 ::real as rdown20,
 			   c.buy::real,
 		       g.low::real,
 		       c.c_down20::real,
@@ -40,11 +47,12 @@ end;
 $function$
 ;
 
+
 -- =========================================================================================================================
 -- =========================================================================================================================
 --储存所有代码的find数据
 
-CREATE OR REPLACE FUNCTION public.find_store(rch int,rb real)
+CREATE OR REPLACE FUNCTION public.find_store(rch integer, rb real,dnlag int)
  RETURNS void
  LANGUAGE plpgsql
 AS $function$
@@ -56,6 +64,7 @@ begin
 		code text, 
 		date date,
 		rchannl real,
+		rdown20 real,
 		buy real,
 		low real,
 		c_down20 real,
@@ -68,11 +77,12 @@ begin
 	    );
    for x in select stock_code.code from stock_code loop
     -- -------------------------------------------------------------------------------------------
-         insert into findall select * from find(x,rch,rb);
+         insert into findall select * from find(x,rch,rb,dnlag);
 	-- -------------------------------------------------------------------------------------------
 	end loop;
     create index findall_index on findall(code,date);
 END
 $function$
 ;
+
 
